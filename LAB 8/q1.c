410b6bd708069ef27cfc6e85909c9939e64e4ecf
AlabasterRig <94524324+AlabasterRig@users.noreply.github.com>
2023-11-02 17:00:27 +0530

    Update q1.c

--- LAB 8/q1.c
+++ LAB 8/q1.c
@@ -1,71 +1,96 @@
-/* Given an undirected weighted connected graph G(V, E) and starring
-vertex ‘s’. Maintain a Min-Priority Queue ‘Q’ from the vertex set V and apply Prim’s algorithm
-to
-• Find the minimum spanning tree T(V, E’). Display the cost adjacency matrix of ‘T’.
-• Display total cost of the minimum spanning tree T.
+/* Given a directed graph G (V, E) and a starting vertex ‘s’.
+● Determine the lengths of the shortest paths from the starting vertex ‘s’ to all other
+vertices in the graph G using Dijkstra’s Algorithm.
+● Display the shortest path from the given source ‘s’ to all other vertices.
 Note# Nodes will be numbered consecutively from 1 to n (user input), and edges will have
-varying weight. The graph G can be read from an input file “inUnAdjMat.dat” that contains cost
-adjacency matrix. The expected output could be displayed as the cost adjacency matrix of the
-minimum spanning tree and total cost of the tree.
+varying distances or lengths. The graph G can be read from an input file “inDiAdjMat1.dat” that
+contains non-negative cost adjacency matrix. The expected output could be as per the sample
+format.
+Content of the input file “inDiAdjMat1.dat” could be
+0 10 0 5 0
+0 0 1 2 0
+0 0 0 0 4
+3 0 9 0 2
+7 0 6 0 0
 
-Content of the input file “inUnAdjMat.dat” could be
-0 4 0 0 0 0 0 8 0
-4 0 8 0 0 0 0 11 0
-0 8 0 7 0 4 0 0 2
-0 0 7 0 9 14 0 0 0
-0 0 0 9 0 10 0 0 0
-0 0 4 14 10 0 2 0 0
-0 0 0 0 0 2 0 1 6
-8 11 0 0 0 0 1 0 7
-0 0 2 0 0 0 6 7 0
 Input:
-Enter the Number of Vertices: 9
-Enter the Starting Vertex: 1
+Enter the Number of Vertices: 5
+Enter the Source Vertex: 1
 Output:
-0 4 0 0 0 0 0 8 0
-4 0 0 0 0 0 0 0 0
-0 0 0 7 0 4 0 0 2
-0 0 7 0 9 0 0 0 0
-0 0 0 9 0 0 0 0 0
-0 0 4 0 0 0 2 0 0
-0 0 0 0 0 2 0 1 0
-8 0 0 0 0 0 1 0 0
-0 0 2 0 0 0 0 0 0
-Total Weight of the Spanning Tree: 37*/
+Source Destination Cost Path
+1 1 0 -
+1 2 8 1->4->2
+1 3 9 1->4->2->3
+1 4 5 1->4
+1 5 7 1->4->5*/
 #include <stdio.h>
 #include <stdbool.h>
 #include <limits.h>
 
 #define MAX_VERTICES 100
 
-// Function to find the minimum vertex with the minimum key value
-int findMinKeyVertex(int key[], bool inMST[], int V) {
-    int minKey = INT_MAX;
-    int minVertex;
+// Function to find the vertex with the minimum distance
+int minDistance(int dist[], bool sptSet[], int V) {
+    int min = INT_MAX, min_index;
 
-    for (int v = 0; v < V; v++) {
-        if (!inMST[v] && key[v] < minKey) {
-            minKey = key[v];
-            minVertex = v;
+    for (int v = 1; v <= V; v++) {
+        if (!sptSet[v] && dist[v] < min) {
+            min = dist[v];
+            min_index = v;
         }
     }
 
-    return minVertex;
+    return min_index;
 }
 
-// Function to display the cost adjacency matrix
-void displayMSTCostMatrix(int costMatrix[][MAX_VERTICES], int parent[], int V) {
-    printf("Cost Adjacency Matrix of the Minimum Spanning Tree:\n");
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            if (parent[j] == i) {
-                printf("%d ", costMatrix[i][j]);
-            } else {
-                printf("0 ");
+// Function to print the shortest path from source to vertex v
+void printPath(int parent[], int v) {
+    if (v == 0) {
+        return;
+    }
+    printPath(parent, parent[v]);
+    printf("%d->", v);
+}
+
+// Function to display the shortest paths and their lengths
+void printShortestPaths(int dist[], int parent[], int V, int source) {
+    printf("Source Destination Cost Path\n");
+    for (int v = 1; v <= V; v++) {
+        if (v != source) {
+            printf("%d %d %d %d->", source, v, dist[v], source);
+            printPath(parent, v);
+            printf("\b\b\n");
+        }
+    }
+}
+
+// Function to perform Dijkstra's algorithm
+void dijkstra(int graph[MAX_VERTICES][MAX_VERTICES], int V, int source) {
+    int dist[MAX_VERTICES];
+    bool sptSet[MAX_VERTICES];
+    int parent[MAX_VERTICES];
+
+    for (int v = 1; v <= V; v++) {
+        dist[v] = INT_MAX;
+        sptSet[v] = false;
+    }
+
+    dist[source] = 0;
+    parent[source] = 0;
+
+    for (int count = 1; count < V; count++) {
+        int u = minDistance(dist, sptSet, V);
+        sptSet[u] = true;
+
+        for (int v = 1; v <= V; v++) {
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && (dist[u] + graph[u][v] < dist[v])) {
+                dist[v] = dist[u] + graph[u][v];
+                parent[v] = u;
             }
         }
-        printf("\n");
     }
+
+    printShortestPaths(dist, parent, V, source);
 }
 
 int main() {
@@ -73,60 +98,27 @@ int main() {
     printf("Enter the Number of Vertices: ");
     scanf("%d", &V);
 
-    int costMatrix[MAX_VERTICES][MAX_VERTICES];
-    FILE *file = fopen("inUnAdjMat.dat", "r");
-    
+    int source;
+    printf("Enter the Source Vertex: ");
+    scanf("%d", &source);
+
+    int graph[MAX_VERTICES][MAX_VERTICES];
+
+    FILE *file = fopen("inDiAdjMat1.dat", "r");
     if (file == NULL) {
         printf("Failed to open the input file.\n");
         return 1;
     }
 
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            fscanf(file, "%d", &costMatrix[i][j]);
+    for (int i = 1; i <= V; i++) {
+        for (int j = 1; j <= V; j++) {
+            fscanf(file, "%d", &graph[i][j]);
         }
     }
 
     fclose(file);
 
-    int startVertex;
-    printf("Enter the Starting Vertex: ");
-    scanf("%d", &startVertex);
-
-    int parent[MAX_VERTICES];
-    int key[MAX_VERTICES];
-    bool inMST[MAX_VERTICES];
-
-    for (int i = 0; i < V; i++) {
-        key[i] = INT_MAX;
-        inMST[i] = false;
-    }
-
-    key[startVertex - 1] = 0;
-    parent[startVertex - 1] = -1;
-
-    for (int count = 0; count < V - 1; count++) {
-        int u = findMinKeyVertex(key, inMST, V);
-        inMST[u] = true;
-
-        for (int v = 0; v < V; v++) {
-            if (costMatrix[u][v] && !inMST[v] && costMatrix[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = costMatrix[u][v];
-            }
-        }
-    }
-
-    displayMSTCostMatrix(costMatrix, parent, V);
-
-    int totalWeight = 0;
-    for (int i = 0; i < V; i++) {
-        if (key[i] != INT_MAX) {
-            totalWeight += key[i];
-        }
-    }
-
-    printf("Total Weight of the Spanning Tree: %d\n", totalWeight);
+    dijkstra(graph, V, source);
 
     return 0;
 }
